56a1e988b171c5550367440b718eb5acaa8b8c30
Anton Morozov <amorozov86@gmail.com>
2022-05-25 22:39:10 +0300

    lesson6: update init_db.php => add pixabay images

--- public/init_db.php
+++ public/init_db.php
@@ -4,32 +4,61 @@ require_once dirname(__DIR__) . '/app/init_app.php';
 use Database\Database;
 use Classes\LoremIpsum;
 
-function createTable($db, $sqlQuery)
+function createTable($sqlQuery)
 {
-    $db->exec($sqlQuery);
-    echo "table created successfully</br>";
+    try {
+        $db = Database::getInstance()->getDb();
+        $request = $db->prepare($sqlQuery);
+        $request->execute();
+        echo "table created successfully</br>";
+    } catch (Exception $exception) {
+        echo $exception->getMessage();
+    }
+
 }
 
-function fillTable($db, $tableName, $rowQuantity)
+function fillTable($rowQuantity)
 {
     $loremIpsum = new LoremIpsum();
     try {
+        $imagesList = getImages();
+        $sqlQuery = "INSERT INTO `goods` (`name`, `description`, `price`, `preview_image`, `big_image`) VALUES (:name, :description, :price, :previewImage, :bigImage);";
+
+        $db = Database::getInstance()->getDb();
+        $request = $db->prepare($sqlQuery);
+
         for ($i = 0; $i < $rowQuantity; $i++) {
-            $name = $loremIpsum->word();
-            $description = $loremIpsum->words(12);
-            $price = rand(1, 100000);
-            $addRowQuery = "INSERT INTO $tableName (`name`, `description`, `price`) VALUES ('$name', '$description', $price);";
-            $db->exec($addRowQuery);
+            $imageIndex = $i >= sizeof($imagesList) ? $i - sizeof($imagesList) : $i;
+            $requestParams = [
+                'name' => $loremIpsum->word(),
+                'description' => $loremIpsum->words(12),
+                'price' => rand(1, 100000),
+                'previewImage' => $imagesList[$imageIndex]['webformatURL'],
+                'bigImage' => $imagesList[$imageIndex]['largeImageURL'],
+            ];
+            $request->execute($requestParams);
         }
-        echo "$tableName filled successfully. Added rows - $rowQuantity</br>";
+        echo "table goods filled successfully. Added rows - $rowQuantity</br>";
     } catch (PDOException $exception) {
         echo $exception->getMessage();
     }
 }
 
+function getImages() {
+    $queryWords = '&q=furniture';
+    $imageType = '&image_type=photo';
+    $orientation = '&orientation=horizontal';
+    $imagesPerPage = '&per_page=200';
+    $requestPath = "https://pixabay.com/api/?key=" . $_ENV['PIXABAY_API'] . $queryWords . $imageType . $orientation . $imagesPerPage;
+
+    $response = file_get_contents($requestPath);
+    $result = json_decode($response, true);
+    return $result['hits'];
+}
+
 $usersTableQuery = "CREATE TABLE `users` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL , `email` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), UNIQUE (`email`)) ENGINE = InnoDB;";
 
-$goodsTableQuery = "CREATE TABLE `goods` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `name` VARCHAR(255) NOT NULL, `description` TEXT NOT NULL, `price` INT UNSIGNED NOT NULL, PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+$goodsTableQuery = "CREATE TABLE `goods` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `name` VARCHAR(127) NOT NULL , `description` TEXT NOT NULL , `price` INT UNSIGNED NOT NULL , `preview_image` VARCHAR(255) NOT NULL , `big_image` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
 
 echo "init_db.php </br>";
 
@@ -37,9 +66,9 @@ if ($_GET['pass'] === $_ENV['DB_PASS']) {
     echo "<h3>you pass</h3>";
     try {
         $db = Database::getInstance()->getDb();
-        createTable($db, $goodsTableQuery);
-        createTable($db, $usersTableQuery);
-        fillTable($db,'goods', 200);
+        createTable($goodsTableQuery);
+        createTable($usersTableQuery);
+        fillTable(200);
     } catch (PDOException $exception) {
         echo $exception->getMessage();
     }
