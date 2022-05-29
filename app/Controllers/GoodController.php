37ab3a8b6f74f733bba1b120d1a8a9fae70387f3
Anton Morozov <amorozov86@gmail.com>
2022-05-29 11:58:21 +0300

    lesson7: update pages, add phpunit

--- app/Controllers/GoodController.php
+++ app/Controllers/GoodController.php
@@ -1,42 +1,34 @@
 <?php
 namespace Controllers;
 
+use Exception;
+
+use Classes\Router;
+use Models\Cart;
 use Models\GoodsList;
 
 class GoodController extends BaseController
 {
-    private int $goodsPerPage = 6;
-
     public function __construct($params)
     {
         parent::__construct($params);
-
-//        $this->goodsPerPage = $goodsPerPage;
         $this->title = 'GoodPage';
         $this->templateFileName = 'good_page.html.twig';
-        var_dump($this->params);
         $this->addContent('good', GoodsList::getGoodById($this->params[0]));
     }
 
-    private function getGoodsList()
-    {
-        $startId = (($this->getCurrentPage() - 1) * $this->goodsPerPage) + 1;
-        return GoodsList::some($startId, $this->goodsPerPage);
-    }
-
-    private function getPagesQuantity(): int
-    {
-        return (int) ceil(GoodsList::allQuantity() / $this->goodsPerPage);
-    }
-
-    private function getCurrentPage(): int
-    {
-        if (isset($_GET['page'])) {
-            return (int) $_GET['page'];
-        } else {
-            return 1;
+    function beforeRender() {
+        if (Router::getRequestMethode() === 'POST') {
+            try {
+                $this->addGoodToCart();
+                $this->addContent('result', 'Товар добавлен');
+            } catch (Exception $exception) {
+                $this->addContent('result', $exception->getMessage());
+            }
         }
     }
 
-    function beforeRender() {}
+    private function addGoodToCart() {
+        Cart::add($this->getUser()->getId(), $_POST['good-id']);
+    }
 }
